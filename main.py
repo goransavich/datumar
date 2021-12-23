7724728405e15c12803bbeb102d053b4349fdf33
Goran Savic <goran.savich@gmail.com>
2021-12-23 12:08:10 +0100

    final

--- main.py
+++ main.py
@@ -12,13 +12,25 @@ Window.size = (350,550)
 class DatumarWidget(Widget):
     label_result = StringProperty()
     def result(self):
-        date_1 = datetime(2002, 12, 1)
-        date_2 = datetime(2021, 10, 1)
-        # Get the interval between two dates
-        diff = relativedelta.relativedelta(date_2, date_1)
-        #('Complete Difference between two dates: ')
-        #diff.years, ' years, ', diff.months, ' months and ', diff.days, ' days')
-        self.label_result = f"{diff.years} godina {diff.months} meseci {diff.days} dana"
+        try:
+            pocetni_dan = self.ids.dan_pocetak.text
+            pocetni_mesec = self.ids.mesec_pocetak.text
+            pocetna_godina = self.ids.godina_pocetak.text
+            zavrsni_dan = self.ids.dan_kraj.text
+            zavrsni_mesec = self.ids.mesec_kraj.text
+            zavrsna_godina = self.ids.godina_kraj.text
+
+            date_1 = datetime(int(pocetna_godina), int(pocetni_mesec), int(pocetni_dan))
+            date_2 = datetime(int(zavrsna_godina), int(zavrsni_mesec), int(zavrsni_dan))
+
+            # Get the interval between two dates
+            diff = relativedelta.relativedelta(date_2, date_1)
+            # ('Complete Difference between two dates: ')
+            self.label_result = f"{diff.years} godina {diff.months} meseci {diff.days} dana"
+        except:
+            self.label_result = "Morate uneti sve ispravne vrednosti"
+
+
 
 class DatumarApp(App):
     def build(self):
